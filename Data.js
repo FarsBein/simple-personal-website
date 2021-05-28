77a7edb633034dce53e42aea7c813af9d97d98eb
Fars Bein <53714581+FarsBein@users.noreply.github.com>
2021-05-27 22:15:34 -0400

    using already downloaded images

--- Data.js
+++ Data.js
@@ -5,25 +5,25 @@ const data =
             name:"GITME",
             url:"https://github.com/FarsBein/GitMe",
             description:"Allows users to create personal websites using their GitHub effortlessly",
-            image:"https://res.cloudinary.com/farsbein01/image/upload/v1622076797/localhost_3000__Max_fvqrcm.png",
+            image:'./images/Gitme.png',
         },
         {
             name:"Maze and A*",
             url:"https://github.com/FarsBein/Maze_A_Star",
             description:"A pygame program that generators random mazes using a backtrack algorithm. It also alows users to solve the maze using the well known A* algorithm",
-            image:"https://res.cloudinary.com/farsbein01/image/upload/v1622078022/raw.githubusercontent.com_FarsBein_Maze_A_Star_master_preview_20images_image_1.JPG_Max_ajl7je.png",
+            image:'./images/maz.png',
         },
         {
             name:"Teamup hub",
             url:"https://github.com/FarsBein/Teemup-hub",
             description:"online advertising service that operates as a centralized network of online communities for posting cooperation requests",
-            image:"https://res.cloudinary.com/farsbein01/image/upload/v1622075996/teamuphub.netlify.app__Max_wdfwde.png",
+            image:'./images/Teamuphub.png',
         },
         {
             name:"Coronavirus Information Centre",
             url:"https://github.com/FarsBein/Coronavirus-Information-Centre",
             description:"Coronavirus Information Centre is a one-stop website for your all covid related information",
-            image:"https://res.cloudinary.com/farsbein01/image/upload/v1622076692/covid-info-center.000webhostapp.com_index.html_Max_j9nq42.png",
+            image:'./images/CIC.png',
         }
     ],
     aboutMe: {
